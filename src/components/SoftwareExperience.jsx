1a16b6f3e5d6c0a80bb3e06554e6c7ca131c6402
thisisalexn <nuss.alex@mail.ru>
2024-12-11 21:16:18 +0100

    Refined Software Experience

--- src/components/SoftwareExperience.jsx
+++ src/components/SoftwareExperience.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Marquee from './basic/Marquee';
-import { FaGitAlt, FaReact, FaJava, FaHtml5, FaCss3Alt, FaGithub, FaGitlab } from "react-icons/fa";
+import { FaGitAlt, FaReact, FaJava, FaHtml5, FaCss3Alt, FaGithub, FaGitlab, FaConfluence } from "react-icons/fa";
 import { SiIntellijidea, SiJira, SiXcode } from "react-icons/si";
 import { FaUnity } from "react-icons/fa6";
 import IconText from './basic/IconText';
@@ -9,9 +9,10 @@ import { VscVscode, VscVscodeInsiders } from "react-icons/vsc";
 const software = [
     <IconText text="IntelliJ IDEA" icon={SiIntellijidea} />,
     <IconText text="GitHub" icon={FaGithub} />,
-    <IconText text="JIRA" icon={SiJira} />,
+    <IconText text="Jira" icon={SiJira} />,
     <IconText text="Unity" icon={FaUnity} />,
     <IconText text="GitLab" icon={FaGitlab} />,
+    <IconText text="Confluence" icon={FaConfluence} />,
     <IconText text="Xcode" icon={SiXcode} />,
     <IconText text="VS Code" icon={VscVscodeInsiders} />,
 ]
